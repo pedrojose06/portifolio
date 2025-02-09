7f91bfc3dd076a0c196500bca015960b2b38dee1
Pedro Moraes <pmoraes@esapiens.com.br>
2025-02-09 20:53:04 -0300

    feat: add animation and jotai for state managment

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -1,5 +1,10 @@
+'use client'
+
 import NavBarItem from '@/app/features/NavBarItem/NavBarItem'
 import NavBarLogo from '@/app/features/NavbarLogo/NavBarLogo'
+import { useAtom } from 'jotai'
+import { AnimatePresence, motion } from 'motion/react'
+import navbarAtom from './atoms/navbar'
 
 const items = [
   { title: 'Home', path: '/' },
@@ -8,15 +13,37 @@ const items = [
 ]
 
 const NavBar = () => {
+  const [show] = useAtom(navbarAtom)
+  // const [show, setShow] = useState(false)
   return (
-    <nav className="border2 flex w-full flex-col items-center justify-between border-b-[0.5px] border-b-primary border-solid bg-background md:h-16 md:flex-row">
+    <AnimatePresence>
       <NavBarLogo />
-      <div className=" flex w-full flex-col items-center justify-center md:w-5/12 md:flex-row">
-        {items.map((item) => (
-          <NavBarItem key={item.title} title={item.title} path={item.path} />
-        ))}
-      </div>
-    </nav>
+      <motion.nav
+        key="box"
+        animate={{ height: show ? 'auto' : 0 }}
+        transition={{ duration: 0.5, delay: show ? 0 : 0.5 }}
+        className="border2 flex w-full flex-col items-center justify-between border-b-[0.5px] border-b-primary border-solid bg-background md:h-16 md:flex-row"
+      >
+        <motion.div
+          animate={{ opacity: show ? 1 : 0 }}
+          transition={{
+            duration: show ? 0.2 : 0.5,
+            delay: show ? 0.5 : 0,
+            ease: [0, 0.71, 0.2, 1.01],
+          }}
+        >
+          <div className=" flex w-full flex-col items-center justify-center md:w-5/12 md:flex-row">
+            {items.map((item) => (
+              <NavBarItem
+                key={item.title}
+                title={item.title}
+                path={item.path}
+              />
+            ))}
+          </div>
+        </motion.div>
+      </motion.nav>
+    </AnimatePresence>
   )
 }
 
--- src/app/features/NavbarLogo/NavBarLogo.tsx
+++ src/app/features/NavbarLogo/NavBarLogo.tsx
@@ -1,11 +1,25 @@
+import navbarAtom from '@/app/components/NavBar/atoms/navbar'
+import { useAtom } from 'jotai'
+import { Fragment } from 'react'
+
 const NavBarLogo = () => {
+  const [show, setShow] = useAtom(navbarAtom)
   return (
-    <a
-      href="/"
-      className="relative flex h-16 w-full items-center justify-center text-3xl text-primary hover:cursor-pointer md:w-1/6 md:justify-start"
-    >
-      <span>./pjtm</span>
-    </a>
+    <Fragment key="logo">
+      <a
+        href="/"
+        className="relative flex h-16 w-full items-center justify-center text-3xl text-primary hover:cursor-pointer md:w-1/6 md:justify-start"
+      >
+        <span>./pjtm</span>
+      </a>
+      <button
+        type="button"
+        className="absolute top-0 right-0 h-0"
+        onClick={() => setShow(!show)}
+      >
+        \/
+      </button>
+    </Fragment>
   )
 }
 export default NavBarLogo
