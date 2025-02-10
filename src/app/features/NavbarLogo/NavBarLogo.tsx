0ed37d0f2ab30ef87af616c820ae2000412a6bb2
Pedro Moraes <pmoraes@esapiens.com.br>
2025-02-09 22:01:46 -0300

    feat: add icon to menu

--- src/app/features/NavbarLogo/NavBarLogo.tsx
+++ src/app/features/NavbarLogo/NavBarLogo.tsx
@@ -1,6 +1,8 @@
 import navbarAtom from '@/app/components/NavBar/atoms/navbar'
 import { useAtom } from 'jotai'
 import { Fragment } from 'react'
+import { MdKeyboardArrowDown } from 'react-icons/md'
+import { motion } from 'motion/react'
 
 const NavBarLogo = () => {
   const [show, setShow] = useAtom(navbarAtom)
@@ -14,10 +16,12 @@ const NavBarLogo = () => {
       </a>
       <button
         type="button"
-        className="absolute top-0 right-0 h-0"
+        className="absolute top-0 right-0 h-16"
         onClick={() => setShow(!show)}
       >
-        \/
+        <motion.div animate={{ rotate: show ? 180 : 0 }}>
+          <MdKeyboardArrowDown className="text-4xl text-primary" />
+        </motion.div>
       </button>
     </Fragment>
   )
