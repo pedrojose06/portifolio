5634d71870c1f8a6a0bf5e123454254834ef6b5e
Pedro Moraes <pmoraes@esapiens.com.br>
2025-02-11 23:35:39 -0300

    feat: include my onfo into about page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,31 +1,48 @@
 const About = () => {
   return (
     <section className="flex h-auto min-h-screenDvh flex-col py-24 md:flex-row">
-      <div className="flex flex-1 flex-col items-center justify-around">
-        <p>
-          Hello! I’m Baptiste and my passion is creating all sorts of things
-          related to new technologies. I’m passionate about Web development and
-          I worked for the first time as a Front-End dev in 2018 at CAUE Du
-          Nord.
+      <div className="flex flex-1 flex-col justify-around">
+        <p className="pt-2">
+          Hello! I’m Pedro Moraes, a Front-End Developer with Full Stack
+          experience, passionate about building user-friendly and
+          high-performance applications. I started my journey in web development
+          in 2019, and since then, I’ve had the opportunity to work on diverse
+          projects across different industries.
         </p>
-        <p>
-          I’ve started university in 2018 at Epitech Lille (school of ….) and
-          since then I’ve had a lot of opportunities to work on various
-          technologies such as React Native, Swift, VueJs, Unity, C++, C# and a
-          lot more. I also have had the opportunity to work as a DevOps during
-          my scholarship.
+        <p className="pt-2">
+          I worked for the first time as a Front-End Developer in 2019 at
+          Moovefy, where I gained hands-on experience in modern web
+          technologies. Over the years, I have contributed to scalable
+          solutions, optimized processes, and led transitions from legacy
+          systems like <span className="text-primary">AngularJS</span> to modern
+          frameworks such as <span className="text-primary">React</span>.
         </p>
-        <p>
-          This year I had the opportunity to spend 6 month in California at
-          CSUSM where my major was Computer Science. I ended this semester with
-          the award of the Outstanding studient for my dedication and work in
-          this university.
+        <p className="pt-2">
+          I’ve also had the opportunity to work with various technologies,
+          including <span className="text-primary">React</span>,{' '}
+          <span className="text-primary">Vue.js</span>,{' '}
+          <span className="text-primary">TypeScript</span>,{' '}
+          <span className="text-primary">JavaScript</span>,{' '}
+          <span className="text-primary">TailwindCSS</span>,{' '}
+          <span className="text-primary">Styled Components</span>,{' '}
+          <span className="text-primary">.NET Core</span>,{' '}
+          <span className="text-primary">C#</span>,{' '}
+          <span className="text-primary">SQL Server</span>,{' '}
+          <span className="text-primary">PostgreSQL</span>,{' '}
+          <span className="text-primary">Azure</span>,{' '}
+          <span className="text-primary">Docker</span>,
+          <span className="text-primary">and more</span>. Additionally, I have
+          experience with testing frameworks like Vitest and Jest, ensuring code
+          quality and performance.
         </p>
-        <p>
-          And finally I started to stream a part of what I do : my training, my
-          projects and my ideas on Twitch. For me it’s a good way to share with
-          other people my journey and maybe to change the way people can see
-          tech jobs & life.
+        <p className="pt-2">
+          Beyond coding, I love collaborating with diverse teams, sharing
+          knowledge, and fostering a positive and inclusive work environment.
+          For me, teamwork, empathy, and creativity are key elements in building
+          great products and impactful solutions.
+        </p>
+        <p className="pt-2">
+          Let’s connect and create something amazing together!
         </p>
       </div>
       <div className=" flex min-h-80 min-w-80 flex-1 items-center justify-center py-8 md:px-8">
