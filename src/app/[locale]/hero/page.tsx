66d58f4a0e4c2944dc9151fb7eb2a1ea13da0a41
Pedro Moraes <pmoraes@esapiens.com.br>
2025-03-02 00:59:37 -0300

    feat: include effect into hero section

--- src/app/[locale]/hero/page.tsx
+++ src/app/[locale]/hero/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 import Button from '@/app/components/Button/Button'
+import Test from '@/app/components/Test/Test'
 import { useTranslations } from 'next-intl'
 
 const Hero = () => {
@@ -7,15 +8,31 @@ const Hero = () => {
   return (
     <section
       id="hero"
-      className="mt-20 mb-8 flex min-h-screenDvh w-full flex-col items-start justify-center font-code text-white md:mt-0 md:h-screenDvh md:w-1/2 md:p-0"
+      className="mt-20 mb-8 flex min-h-screenDvh w-full flex-col items-start justify-center font-code text-white md:mt-0 md:h-screenDvh md:p-0"
     >
       <div className="text-primary text-xs">{t('title')}</div>
-      <div className="py-5 font-fjalla text-6xl text-blue-100">
+      <div className="w-full py-5 font-fjalla text-5xl text-blue-100 md:text-6xl">
         Pedro Moraes.
         <br />
-        <span className="font-fjalla text-blue-200">
+        <div className="flex flex-wrap font-fjalla text-blue-200 ">
           {t('currentPosition')}
-        </span>
+          <Test
+            texts={[
+              'Frontend Engineer.',
+              'React Developer.',
+              'FullStack Engineer.',
+            ]}
+            mainClassName="font-fjalla text-blue-200 font-bold md:pl-2"
+            staggerFrom={'last'}
+            initial={{ y: '100%' }}
+            animate={{ y: 0 }}
+            exit={{ y: '-120%' }}
+            staggerDuration={0.025}
+            splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+            transition={{ type: 'spring', damping: 30, stiffness: 400 }}
+            rotationInterval={3000}
+          />
+        </div>
       </div>
       <div className="text-lg">{t('synopsis')}</div>
       <div className="flex w-full justify-center pt-8 md:justify-start md:pt-16">
